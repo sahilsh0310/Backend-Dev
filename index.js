67b44962a31a9653a13e528e259fc198f52bebc5
sahilsh0310 <sahil.sh0310@gmail.com>
2026-01-19 11:39:43 +0530

    Task 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const rawUsers = [
+  { id: 1, name: "Rahul", password: "fb_password", role: "admin" },
+  { id: 2, name: "Sanya", password: "123_password", role: "user" },
+  { id: 3, name: "Amit", password: "secret_password", role: "user" }
+];
+
+// 1. Remove password using map() and rest operator
+const safeUsers = rawUsers.map(({ password, ...rest }) => rest);
+
+// 2. Filter only admins
+const admins = safeUsers.filter(user => user.role === "admin");
+
+console.log("Safe Users:", safeUsers);
+console.log("Admins:", admins);
