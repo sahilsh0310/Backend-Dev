0e92dbaaacd2223350be5139b61b1c90f5348428
Sahil Sharma <sahil.sh0310@gmail.com>
2026-02-05 10:58:49 +0530

    Delete classroom_practice directory

--- classroom_practice/Nodejs-fundamentals/DAY_1/app.js
+++
@@ -1,5 +0,0 @@
-// Import custom logger module
-const logActivity = require("./logger")
-logActivity("Application started");
-logActivity("User logged in");
-logActivity("An error occurred");
--- classroom_practice/Nodejs-fundamentals/DAY_1/logger.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-function logActivity(message){
-    const time=newDate().toLocaleString();
-    fs.appendFileSync("activity.log",time+"-"+message+"\n");
-}
-  MediaSourceHandle.export=logActivity;
